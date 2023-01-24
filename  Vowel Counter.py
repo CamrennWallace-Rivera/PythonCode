2a151af284fa449745d16aa1f14abee5cafc5410
CamrennWallace-Rivera <107094741+CamrennWallace-Rivera@users.noreply.github.com>
2023-01-23 23:16:19 -0500

    Add files via upload

---
+++  Vowel Counter.py
@@ -0,0 +1,19 @@
+#Camrenn Wallace-Rivera
+def vowels(string):
+    count=0
+    for i in range(len(string)):
+        if(string[i] in "aeiou"):
+            count+=1
+    return count
+
+def consonants(string):
+    count=0
+    for i in range(len(string)):
+        if(string[i] not in "aeiou"):
+            count+=1
+    return count
+string=input("Enter String: ")
+print("Vowels: ",vowels(string))
+print("Consonants: ",consonants(string))
+print("Vowels: ",vowels(string))
+print("Consonants: ",consonants(string))
